2f8b84fa48e7003c94f989fdd69d5fb135780ac3
HowlyBlood <tiitou22@gmail.com>
2021-01-12 22:51:28 +0100

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,225 @@
+Raidlist={
+    # NORMAL
+
+    'nSS':{
+        "fr_name":"Sollance - Normal",
+        "en_name":"Sunspire - Normal",
+        "requirements":"1,2,9",
+        "DLC":"Elsweyr",
+        "fr_sets":"\
+L’Œil de Nahviintaas (léger) : https://esofinder.com/fr/sets/443\n\
+Le Dévot du Faux dieu (léger) : https://esofinder.com/fr/sets/444\n\
+La Dent de Lokkestiiz (moyen) : https://esofinder.com/fr/sets/445\n\
+La griffe d’Yolnahkriin (lourd) : https://esofinder.com/fr/sets/446"
+    },
+    'nCR':{
+        "fr_name":"Pas-des-nués - Normal",
+        "en_name":"Cloudrest - Normal",
+        "requirements":"1,2,9",
+        "DLC":"Summerset",
+        "fr_sets":"\
+Manteau de Siroria (léger) : https://esofinder.com/fr/sets/390\n\
+Vêture d'Olorimë (léger) : https://esofinder.com/fr/sets/391\n\
+Armes de Relequen (moyen) : https://esofinder.com/fr/sets/389\n\
+Égide de Galenwe (lourd) : https://esofinder.com/fr/sets/388"
+    },
+    'nKA':{
+        "fr_name":"Égide de Kyne - Normal",
+        "en_name":"Kyne’s Aegis - Normal",
+        "requirements":"1,2,9",
+        "DLC":"Greymoor",
+        "fr_sets":"\
+Le Vent de Kyne (léger) : https://esofinder.com/fr/sets/492\n\
+Commandement de Vrol (léger) : https://esofinder.com/fr/sets/494\n\
+L’Opportuniste rugissant (moyen): https://esofinder.com/fr/sets/496\n\
+La Puissance d’Yandir (lourd) : https://esofinder.com/fr/sets/498"
+    },
+    'nHoF':{
+        "fr_name":"Les salles de la Fabrication - Normal",
+        "en_name":"Hall of Fabrication - Normal",
+        "requirements":"1,2,9",
+        "DLC":"Morrowind",
+        "fr_sets":"\
+Le maître architecte (léger) : https://esofinder.com/fr/sets/332\n\
+La Garde de l'inventeur (léger) : https://esofinder.com/fr/sets/333\n\
+La machine de guerre (moyen) : https://esofinder.com/fr/sets/331\n\
+La Défense automatique (lourd) : https://esofinder.com/fr/sets/330"
+    },
+    'nAS': {
+        "fr_name": "Asile Sanctuaire - Normal",
+        "en_name": "Asylum Sanctorium - Normal",
+        "requirements": "1,2,9",
+        "DLC": "Clockwork City",
+        "fr_sets":"\
+Entaille disciplinée (Armes à deux mains) : https://esofinder.com/fr/sets/363\n\
+Position défensive (Une main et Bouclier) : https://esofinder.com/fr/sets/364\n\
+Tourbillon chaotique (Deux armes) : https://esofinder.com/fr/sets/365\n\
+Jaillissement perforant (Arc) : https://esofinder.com/fr/sets/366\n\
+Force concentrée (Bâton de Destruction) : https://esofinder.com/fr/sets/367\n\
+Bénédiction intemporelle (Bâton de Rétablissement) : https://esofinder.com/fr/sets/368"
+    },
+    'nMoL': {
+        "fr_name": "La gueule de Lorkhaj - Normal",
+        "en_name": "Maw of Lorkhaj - Normal",
+        "requirements": "1,2,9",
+        "DLC": "Thieves's Guild",
+        "fr_sets":"\
+Le Remède du crépuscule (léger) : https://esofinder.com/fr/sets/229\n\
+Danselune (léger) : https://esofinder.com/fr/sets/230\n\
+Le Rugissement d'Alkosh (moyen) : https://esofinder.com/fr/sets/232\n\
+Bastion lunaire (lourd) : https://esofinder.com/fr/sets/231"
+    },
+    'nSO': {
+        "fr_name": "Sanctum Ophidia - Normal",
+        "en_name": "Sanctum Ophidia - Normal",
+        "requirements": "1,2,9",
+        "DLC": "Vanilla",
+        "fr_sets":"\
+Le Mage avisé (léger) : https://esofinder.com/fr/sets/139\n\
+L’Infaillible Mage (léger) : https://esofinder.com/fr/sets/172\n\
+Le Serpent à deux crocs (moyen) : https://esofinder.com/fr/sets/144\n\
+Le Cruel Serpent (moyen) : https://esofinder.com/fr/sets/173\n\
+Le Guerrier immortel (lourd) : https://esofinder.com/fr/sets/136\n\
+L’Éternel Guerrier (lourd) : https://esofinder.com/fr/sets/171"
+    },
+    'nHRC': {
+        "fr_name": "La Citadelle d’Hel Ra - Normal",
+        "en_name": "Hel Ra Citadel - Normal",
+        "requirements": "1,2,9",
+        "DLC": "Vanilla",
+        "fr_sets":"\
+Le Mage destructeur (léger) : https://esofinder.com/fr/sets/143\n\
+L’Infaillible Mage (léger) : https://esofinder.com/fr/sets/172\n\
+Le Serpent venimeux (moyen) : https://esofinder.com/fr/sets/143\n\
+Le Cruel Serpent (moyen) : https://esofinder.com/fr/sets/173\n\
+Le Guerrier furieux (lourd) : https://esofinder.com/fr/sets/137\n\
+L’Éternel Guerrier (lourd) : https://esofinder.com/fr/sets/171"
+    },
+    'nAA': {
+        "fr_name": "L’Archive Aetherienne - Normal",
+        "en_name": "Aetherian Archive - Normal",
+        "requirements": "1,2,9",
+        "DLC": "Vanilla",
+        "fr_sets":"\
+Le Mage guérisseur (léger) : https://esofinder.com/fr/sets/141\n\
+L’Infaillible Mage (léger) : https://esofinder.com/fr/sets/172\n\
+Le Serpent rapide (moyen) : https://esofinder.com/fr/sets/142\n\
+Le Cruel Serpent (moyen) : https://esofinder.com/fr/sets/173\n\
+Le Guerrier défenseur (lourd) : https://esofinder.com/fr/sets/138\n\
+L’Éternel Guerrier (lourd) : https://esofinder.com/fr/sets/171"
+    },
+
+    #VETERAN
+    'vSS':{
+        "fr_name":"Sollance - Veteran",
+        "en_name":"Sunspire - Veteran",
+        "requirements":"1,2,9",
+        "DLC":"Elsweyr",
+        "fr_sets":"\
+L’Œil parfait de Nahviintaas (léger) : https://esofinder.com/fr/sets/448\n\
+Le Dévot parfait du faux dieu (léger) : https://esofinder.com/fr/sets/449\n\
+Dent parfaite de Lokkestiiz (moyen): https://esofinder.com/fr/sets/450\n\
+La Griffe de Lokkestiiz parfaite (lourd) : https://esofinder.com/fr/sets/451"
+    },
+    'vCR':{
+        "fr_name":"Pas-des-nués - Veteran",
+        "en_name":"Cloudrest - Veteran",
+        "requirements":"1,2,9",
+        "DLC":"Summerset",
+        "fr_sets":"\
+Manteau perfectionné de Siroria (léger) : https://esofinder.com/fr/sets/394\n\
+La vêture perfectionnée d'Olorimë (léger) : https://esofinder.com/fr/sets/395\n\
+Armes perfectionnées de Relequen (moyen) : https://esofinder.com/fr/sets/393\n\
+Égide perfectionnée de Galenwe (lourd) : https://esofinder.com/fr/sets/392"
+    },
+    'vKA':{
+        "fr_name":"Égide de Kyne - Normal",
+        "en_name":"Kyne’s Aegis - Normal",
+        "requirements":"1,2,9",
+        "DLC":"Greymoor",
+        "fr_sets":"\
+Le Vent de Kyne perfectionné (léger) : https://esofinder.com/fr/sets/493\n\
+Le Commandement de Vrol perfectionné (léger) : https://esofinder.com/fr/sets/495\n\
+L’Opportuniste rugissant perfectionné (moyen) : https://esofinder.com/fr/sets/497\n\
+La Puissance d’Yandir perfectionnée (lourd) : https://esofinder.com/fr/sets/499"
+    },
+    'vHoF':{
+        "fr_name":"Les salles de la Fabrication - Veteran",
+        "en_name":"Hall of Fabrication - Veteran",
+        "requirements":"1,2,9",
+        "DLC":"Morrowind",
+        "fr_sets":"\
+Le maître architecte (léger) : https://esofinder.com/fr/sets/332\n\
+La Garde de l'inventeur (léger) : https://esofinder.com/fr/sets/333\n\
+La machine de guerre (moyen) : https://esofinder.com/fr/sets/331\n\
+La Défense automatique (lourd) : https://esofinder.com/fr/sets/330"
+    },
+    'vAS': {
+        "fr_name": "Asile Sanctuaire - Veteran",
+        "en_name": "Asylum Sanctorium - Veteran",
+        "requirements": "1,2,9",
+        "DLC": "Clockwork City",
+        "fr_sets":" \
+Entaille disciplinée perfectionnée (Armes à deux mains) : https://esofinder.com/fr/sets/357\n\
+Position défensive perfectionnée (Une main et Bouclier) : https://esofinder.com/fr/sets/358\n\
+Tourbillon chaotique perfectionné (Deux armes) : https://esofinder.com/fr/sets/359\n\
+Jaillissement perforant perfectionné (Arc) : https://esofinder.com/fr/sets/360\n\
+Force concentrée perfectionnée (Bâton de Destruction) : https://esofinder.com/fr/sets/361\n\
+Bénédiction intemporelle perfectionnée (Bâton de Rétablissement) : https://esofinder.com/fr/sets/362"
+    },
+    'vMoL': {
+        "fr_name": "La gueule de Lorkhaj - Veteran",
+        "en_name": "Maw of Lorkhaj - Veteran",
+        "requirements": "1,2,9",
+        "DLC": "Thieves's Guild",
+        "fr_sets":"\
+Le Remède du crépuscule (léger) : https://esofinder.com/fr/sets/229\n\
+Danselune (léger) : https://esofinder.com/fr/sets/230\n\
+Le Rugissement d'Alkosh (moyen) : https://esofinder.com/fr/sets/232\n\
+Bastion lunaire (lourd) : https://esofinder.com/fr/sets/231"
+    },
+    'vSO': {
+        "fr_name": "Sanctum Ophidia - Veteran",
+        "en_name": "Sanctum Ophidia - Veteran",
+        "requirements": "1,2,9",
+        "DLC": "Vanilla",
+        "fr_sets":"\
+Le Mage avisé (léger) : https://esofinder.com/fr/sets/139\n\
+L’Infaillible Mage (léger) : https://esofinder.com/fr/sets/172\n\
+Le Serpent à deux crocs (moyen) : https://esofinder.com/fr/sets/144\n\
+Le Cruel Serpent (moyen) : https://esofinder.com/fr/sets/173\n\
+Le Guerrier immortel (lourd) : https://esofinder.com/fr/sets/136\n\
+L’Éternel Guerrier (lourd) : https://esofinder.com/fr/sets/171"
+    },
+    'vHRC': {
+        "fr_name": "La Citadelle d’Hel Ra - Veteran",
+        "en_name": "Hel Ra Citadel - Veteran",
+        "requirements": "1,2,9",
+        "DLC": "Vanilla",
+        "fr_sets":"\
+Le Mage destructeur (léger) : https://esofinder.com/fr/sets/143\n\
+L’Infaillible Mage (léger) : https://esofinder.com/fr/sets/172\n\
+Le Serpent venimeux (moyen) : https://esofinder.com/fr/sets/143\n\
+Le Cruel Serpent (moyen) : https://esofinder.com/fr/sets/173\n\
+Le Guerrier furieux (lourd) : https://esofinder.com/fr/sets/137\n\
+L’Éternel Guerrier (lourd) : https://esofinder.com/fr/sets/171"
+    },
+    'vAA': {
+        "fr_name": "L’Archive Aetherienne - Veteran",
+        "en_name": "Aetherian Archive - Veteran",
+        "requirements": "1,2,9",
+        "DLC": "Vanilla",
+        "fr_sets":"\
+Le Mage guérisseur (léger) : https://esofinder.com/fr/sets/141\n\
+L’Infaillible Mage (léger) : https://esofinder.com/fr/sets/172\n\
+Le Serpent rapide (moyen) : https://esofinder.com/fr/sets/142\n\
+Le Cruel Serpent (moyen) : https://esofinder.com/fr/sets/173\n\
+Le Guerrier défenseur (lourd) : https://esofinder.com/fr/sets/138\n\
+L’Éternel Guerrier (lourd) : https://esofinder.com/fr/sets/171"
+    },
+}
+Dungeonlist={
+    'nDJ':{
+
+    }
+}
